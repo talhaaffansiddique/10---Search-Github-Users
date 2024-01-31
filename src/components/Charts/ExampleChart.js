9a212a1ab7d9141b38d20a305f6b2aa73923183b
Talha Affan Siddique <talhaaffansiddique@gmail.com>
2024-01-31 09:59:58 +0500

    286 - Fusion Charts Pie Chart

--- src/components/Charts/ExampleChart.js
+++ src/components/Charts/ExampleChart.js
@@ -21,27 +21,18 @@ ReactFC.fcRoot(FusionCharts, Chart, FusionTheme);
 const ChartComponent = ({ data }) => {
   // STEP 3 - Creating the JSON object to store the chart configurations
   const chartConfigs = {
-    type: "column2d", // The chart type
+    type: "pie3d", // The chart type
     width: "400", // Width of the chart
     height: "400", // Height of the chart
     dataFormat: "json", // Data type
     dataSource: {
       // Chart Configuration
       chart: {
-        //Set the chart caption
-        caption: "Countries With Most Oil Reserves [2017-18]",
-        //Set the chart subcaption
-        subCaption: "In MMbbl = One Million barrels",
-        //Set the x-axis name
-        xAxisName: "Country",
-        //Set the y-axis name
-        yAxisName: "Reserves (MMbbl)",
-        numberSuffix: "K",
-        //Set the theme for your chart
+        caption: "languages",
         theme: "fusion",
       },
       // Chart Data
-      data,
+      data: data,
     },
   };
   return <ReactFC {...chartConfigs} />;
